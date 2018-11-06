b559e65ea0445a9527804282b5eb631ee81fa0d6
Clay190 <31509660+Clay190@users.noreply.github.com>
2018-11-06 18:47:56 -0500

    Updated from Brython Server: 11/6/2018 6:47:51 PM

--- CalculusProject.py
+++ CalculusProject.py
@@ -1,1 +1,12 @@
 
+#Clay Kynor and Glen Passow
+#11.17.18
+#CalculusProject.py
+
+d = int(input('Please enter what type of function you have: 1. polynomial '))
+
+if d == 1:
+    n = int(input('Enter the highest degree in your polynomial: '))
+    for i in range (0,n):
+        a = int(input('Please enter the coefficient of the ' + str(i) + ' term '))
+        
