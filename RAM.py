ecbb1b103ae7c43927e752224e3c5ed5aaaa518d
Clay190 <31509660+Clay190@users.noreply.github.com>
2018-11-19 13:04:26 -0500

    Updated from Brython Server: 11/19/2018 1:04:24 PM

--- RAM.py
+++ RAM.py
@@ -1,1 +1,18 @@
 
+#Clay Kynor
+#11.19.18
+
+func = input("Input your function: ")
+num = int(input("Input how many rectangles you want "))
+a = int(input("Input the start of your range "))
+b = int(input("input the end of your range "))
+
+n = (b-a)/num
+total = 0
+
+for i in range(0,num):
+    x=i*(a+n)
+    total += eval(func)*n
+    print(eval(func)*n)*n
+    
+print(total)
