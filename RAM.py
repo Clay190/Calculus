9cecfd14755172802f03a901c0f72fd4f5f01a4b
Clay190 <31509660+Clay190@users.noreply.github.com>
2018-12-04 13:07:42 -0500

    Updated from Brython Server: 12/4/2018 1:07:41 PM

--- RAM.py
+++ RAM.py
@@ -14,19 +14,28 @@ n = (b-a)/num
 Ltotal = 0
 Mtotal = 0
 Rtotal = 0
+SimpTotal = 0
+y1 = 0
+y2 = 0
+y3 = 0
 
 for i in range(0,num):
     x=(i*n)+a
     Ltotal += eval(func)*n
-
-for i in range(1,num+1):
-    x=(i*n)+a
-    Rtotal += eval(func)*n
-    
-for i in range(0,num):
+    y1 = eval(func)
     x = ((i*n)+a) + (n/2)
     Mtotal += eval(func)*n
+    y2 = eval(func)
+    x=((i+1)*n)+a
+    Rtotal += eval(func)*n
+    y3 = eval(func)
+    SimpTotal += (y1+(4*y2)+y3)*(n/6)
+
+
+TrapTotal = (Ltotal+Rtotal)/2
 
 print("LRAM", Ltotal)
 print("MRAM", Mtotal)
 print("RRAM", Rtotal)
+print("Trapezoid Area", TrapTotal)
+print("Simpson's Area", SimpTotal)
