9cf5194f03dddb501d4f7b9f7687ebbe4ad5046e
Clay190 <31509660+Clay190@users.noreply.github.com>
2019-05-24 08:27:09 -0400

    Updated from Brython Server: 5/24/2019 8:27:08 AM

--- RAM.py
+++ RAM.py
@@ -8,7 +8,10 @@ from math import log, log10, sqrt, log2
 func = input("Input your function: ")
 num = int(input("Input how many rectangles you want "))
 a = float(input("Input the start of your range "))
-b = float(input("input the end of your range "))
+b = float(input("Input the end of your range "))
+
+#new variable addition to code
+integral = float(input("Input the integral of your function from a to b "))
 
 n = (b-a)/num
 Ltotal = 0
@@ -31,11 +34,22 @@ for i in range(0,num):
     y3 = eval(func)
     SimpTotal += (y1+(4*y2)+y3)*(n/6)
 
-
 TrapTotal = (Ltotal+Rtotal)/2
+
+#New code finding the percent error of each method
+error1 = abs(((Ltotal - integral)/integral) * 100)
+error2 = abs(((Rtotal - integral)/integral) * 100)
+error3 = abs(((Mtotal - integral)/integral) * 100)
+error4 = abs(((SimpTotal - integral)/integral) * 100)
+error5 = abs(((TrapTotal - integral)/integral) * 100)
 
 print("LRAM", Ltotal)
 print("MRAM", Mtotal)
 print("RRAM", Rtotal)
 print("Trapezoid Area", TrapTotal)
 print("Simpson's Area", SimpTotal)
+print("Percent Error for LRAM is", error1)
+print("Percent Error for RRAM is", error2)
+print("Percent Error for MRAM is", error3)
+print("Percent Error for Trapezoidal Area is", error4)
+print("Percent Error for Simpson's Rule is", error5)
